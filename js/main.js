15586675d7ef7f115c0f63167baefc77a8d974d1
The Creator <diegorazuri@gmail.com>
2017-02-24 20:29:02 -0500

    home terminado

---
+++ js/main.js
@@ -0,0 +1,95 @@
+$(document).ready(function(){
+	
+	$(".btnC").on("click", function(e){
+		e.preventDefault();
+		let idBtn = $(this).attr("id");
+		
+		let idCollectionSelected = "";
+		switch(idBtn){
+			case "btnC1": idCollectionSelected = "#c1";
+				break;
+			case "btnC2": idCollectionSelected = "#c2";
+				break;
+			case "btnC3": idCollectionSelected = "#c3";
+				break;
+			case "btnC4": idCollectionSelected = "#c4";
+				break;
+			case "btnC5": idCollectionSelected = "#c5";
+				break;
+			case "btnC6": idCollectionSelected = "#c6";
+				break;
+				
+			case "btnC7": idCollectionSelected = "#c7";
+				break;
+		}
+
+		$(".galleryGrid").each(function(){
+			if($(this).hasClass("galleryGridSelected") ){
+				$(this).fadeOut();
+				$(this).removeClass("galleryGridSelected");
+				
+			}
+		});
+		$(".btnCollectionList").each(function(){
+			if($(this).hasClass("collectionSelected") ){
+				$(this).removeClass("collectionSelected");
+				
+			}
+		});
+
+		idListSelection = idCollectionSelected.substr(2,1)
+
+		$("#btnC"+ idListSelection).parent().addClass("collectionSelected");
+
+		$(idCollectionSelected).fadeIn();
+		$(idCollectionSelected).addClass("galleryGridSelected")
+
+
+	});
+
+	$("#btnMenu").on("click", function(){
+		$(".navDisplay").slideToggle();
+	});
+
+
+	$('#btnCollections').on('click', function(){
+		idBtn = this.id;
+		s = -20;
+		scroller(idBtn, s)
+	});
+	$('#btnLifestyle').on('click', function(){
+		idBtn = this.id;
+		s = -20;
+		scroller(idBtn, s)
+	});
+	$('#btnAboutus').on('click', function(){
+		idBtn = this.id;
+		s = -100;
+		scroller(idBtn, s)
+	});
+	$('#btnContact').on('click', function(){
+		idBtn = this.id;
+		s = -20;
+		scroller(idBtn, s)
+	});
+
+
+	function scroller(idBtn, s){
+		var strAncla = $("#"+idBtn).attr('data-direct');
+		var presize = $(strAncla).offset().top
+		
+		if (screen.width <500){
+			var size = presize-50;
+		}else{
+
+			var size = presize + s;
+		}
+
+		$(".navDisplay").slideToggle();
+
+		$('body, html').stop(true, true).animate({
+			scrollTop: size
+		}, 1000);
+
+	}
+});
